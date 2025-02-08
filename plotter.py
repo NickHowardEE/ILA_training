563a90dc81c45b176bac57b7b54fa7cfe0903d5e
NickHowardEE <enenen10@gmail.com>
2025-02-08 21:26:52 +0000

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -3,7 +3,7 @@
 import plotly.graph_objects as go
 import numpy as np
 # Custom libraries.
-import ILA_training.beameqs as beamqs
+import ILA_training.beameqs as beaemqs
 
 def plot_deflection(P, beam, scale_factor):
     # Generate x values
