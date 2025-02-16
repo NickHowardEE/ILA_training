87f2ff855029bc296f31378873e6be003e0fea56
NickHowardEE <enenen10@gmail.com>
2025-02-16 19:41:10 +0000

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,9 +1,10 @@
 # ~~~ IMPORTS ~~~
 # Python libraies.
+import math
 import plotly.graph_objects as go
 import numpy as np
 # Custom libraries.
-import ILA_training.beameqs as beameqs
+import beameqs
 
 def plot_deflection(P, beam, scale_factor):
     # Generate x values
@@ -21,4 +22,61 @@ def plot_deflection(P, beam, scale_factor):
         xaxis_range=[0, max(x)],  # Set x-axis limits
         yaxis_range=[0, max(y)+10]   # Set y-axis limits
     )
+    fig.show()
+
+def plot_frequency(nf,beam):
+    period = 1/nf
+    frame_frequency = 0.1
+    frames_per_period = period / frame_frequency
+    print (frames_per_period)
+    num_cycles = 10 # Number of cycles in animation.
+    num_frames = int(num_cycles*frames_per_period) # Number of frames in the animation.
+    num_beam_elements = 10 # Number of elements in the beam.
+
+    # Generate x and y data structures.
+    y_data = np.linspace(0, beam.L, num_beam_elements) # y_data is the distance from the fixed end of the beam.
+    mode_displacements = []
+    for y in y_data:
+        mode_displacements.append(beameqs.mode_shape(y, beam))
+    x_data = [] # x_data is time varying.
+    # Calculate the time varying x data.
+    for i in range(0, num_frames):
+        period = num_frames / num_cycles
+        multiplier = math.sin((2*math.pi/period)*i) # The deflection is based on a sinusoidial function for animation.
+        x_data.append([multiplier*i for i in mode_displacements])
+            
+    # Create the animated plot.
+    fig = go.Figure(
+        data=[go.Scatter(x=x_data[0], y=y_data)],
+        layout=go.Layout(
+            xaxis=dict(range=[-5, 5]),
+            yaxis=dict(range=[0, beam.L + beam.L/5]),
+            title="Animated Natural Frequency",
+            updatemenus=[
+                dict(
+                    type="buttons",
+                    buttons=[
+                        dict(
+                            label="Play",
+                            method="animate",
+                            args=[
+                                None,
+                                {
+                                    "frame": {"duration": 90, "redraw": True},  # Adjust frame duration
+                                    "fromcurrent": True,
+                                    "loop:": True,
+                                    "transition": {"duration": 90, "easing": "linear"},  # Add transition
+                                },
+                            ],
+                        )
+                    ],
+                )
+            ],
+        ),
+        frames=[
+            go.Frame(data=[go.Scatter(x=x_data[i], y=y_data)])
+            for i in range(1, num_frames)
+        ],
+    )
+
     fig.show()
