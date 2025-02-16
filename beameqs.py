e5c7e4da2562523182487d99175ecbfb0bc0ea7a
NickHowardEE <enenen10@gmail.com>
2025-02-16 21:38:34 +0000

    Update beameqs.py

--- beameqs.py
+++ beameqs.py
@@ -27,7 +27,7 @@ class Beam:
     def mass_of_chs(self):
         volume = math.pi * (self.D_outer**2 - self.D_inner**2) / 4 * self.L
         return volume * self.material.density
-        
+    
     def natural_frequency(self):
         mul = self.mass/self.L # mass per unit length
         return (1.875**2 / (2 * math.pi)) * ((self.material.E * self.I) / (mul * self.L**4))**0.5
@@ -39,13 +39,26 @@ class Beam:
 def deflection(x, P, beam):
     return (P * x**2) / (6 * beam.material.E * beam.I) * (3 * beam.L - x)
 
+# Equation to calculate mode shape of cantilever beam.
+def mode_shape(x, beam):
+    l = 1.875
+    r = (math.cosh(l)+math.cos(l))/(math.sinh(l)+math.sin(l))
+    e = 1.0
+    max_val = math.cosh(l*e)-math.cos(l*e)-r*(math.sinh(l*e)-math.sin(l*e))
+    e = x/beam.L
+    d = math.cosh(l*e)-math.cos(l*e)-r*(math.sinh(l*e)-math.sin(l*e))
+    #print (e,d/max_val)
+    return d/max_val
 
-
-
-
+# Equation to calculate bending stress in a beam from an applied moment.
 def bending_stress(M, D, I):
     y = D / 2
     return (M * y / I)
+
+# Equation to calculate applied moment from a bending stress.
+def moment_from_stress(sigma, D, I):
+    y = D / 2
+    return (sigma * I / y)
 
 def fatigue_damage(DEM, SN_curve):
     return 1
